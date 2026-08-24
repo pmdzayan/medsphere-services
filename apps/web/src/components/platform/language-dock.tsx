a336405753cf4c381ed36f60345a9ad24877b4c0
pmdzayan <pmdzayan2004@gmail.com>
2026-08-24 18:23:46 +0000

    fix: format localization language dock

--- apps/web/src/components/platform/language-dock.tsx
+++ apps/web/src/components/platform/language-dock.tsx
@@ -4,7 +4,10 @@ import { LanguageSelector } from '@/components/language-selector';
 
 export function PlatformLanguageDock() {
   return (
-    <div className="fixed bottom-24 right-4 z-40 lg:bottom-6 lg:right-6" aria-label="Application language">
+    <div
+      className="fixed bottom-24 right-4 z-40 lg:bottom-6 lg:right-6"
+      aria-label="Application language"
+    >
       <LanguageSelector />
     </div>
   );
